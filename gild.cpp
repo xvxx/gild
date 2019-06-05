f64bc983358004cf0af691d4e90bd5b092b5f041
dvkt <c@dvkt.io>
2019-06-05 16:38:53 -0700

    use SEND for requests

--- gild.cpp
+++ gild.cpp
@@ -1,33 +1,21 @@
-#include <arpa/inet.h>
 #include <cstring>
 #include <iostream>
-#include <netdb.h>
 #include <signal.h>
 #include <string>
 #include <sys/ioctl.h>
 #include <unistd.h>
-#include <unordered_map>
 
 using namespace std;
-
-static unordered_map<string, string> response_cache;
-
 
 #ifndef ldpl_number
 #define ldpl_number double
 #endif
 
-string GOPHER_HOST;      // gopher.host
-ldpl_number GOPHER_PORT; // gopher.port 
-string GOPHER_SELECTOR;  // gopher.selector
-string GOPHER_RESPONSE;  // gopher.response
 ldpl_number WINDOW_ROWS; // window.rows
 ldpl_number WINDOW_COLS; // window.cols
 
 // module functions
 void error(const string & msg);
-int connect_to(string & hostname, int port);
-string fetch(int sock, string & selector);
 void signal_handler(int sig);
 void set_term_size();
 
@@ -35,21 +23,6 @@ void GOPHER_INIT()
 {
     signal(SIGWINCH, signal_handler);
     set_term_size();
-}
-
-void GOPHER_GET()
-{
-    string cache_key = GOPHER_HOST+":"+to_string(GOPHER_PORT)+GOPHER_SELECTOR;
-    if(response_cache.find(cache_key)!=response_cache.end())
-    {
-        GOPHER_RESPONSE = response_cache[cache_key];
-        return;
-    }
-
-    int sock = connect_to(GOPHER_HOST, GOPHER_PORT);
-    GOPHER_RESPONSE = fetch(sock, GOPHER_SELECTOR);
-    response_cache[cache_key] = GOPHER_RESPONSE;
-    close(sock);
 }
 
 void error(const string & msg)
@@ -73,58 +46,4 @@ void set_term_size()
     WINDOW_COLS = (ldpl_number)w.ws_col;
 }
 
-int connect_to(string & hostname, int port)
-{
-    struct hostent *h = gethostbyname(hostname.c_str());
-    if (h==NULL) { error("bad hostname: "+hostname); }
-    if (h->h_length <= 0) error("gethostbyname() failed");
-
-    char *servIP = inet_ntoa(*(struct in_addr*)(h->h_addr_list[0]));
-
-    int sock;
-    if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
-        error("socket() failed");
-
-    struct sockaddr_in servAddr;
-    memset(&servAddr, 0, sizeof(servAddr)); // zero out
-    servAddr.sin_family      = AF_INET;
-    servAddr.sin_addr.s_addr = inet_addr(servIP);
-    servAddr.sin_port        = htons(port);
-
-    if(connect(sock,(struct sockaddr*)&servAddr,sizeof(servAddr)) < 0)
-        error("connect() failed host:"+hostname+" port:"+to_string(port));
-
-    return sock;
-}
-
-string fetch(int sock, string & selector)
-{
-    string request = "";
-    if (!selector.empty()) request = selector;
-    request += "\r\n";
-
-    if (send(sock,request.c_str(),request.size(),0) != request.size())
-        error("send() sent unexpected number of bytes");
-
-    int bsize = 999999; // XXX 999k? bigger?
-    int bytes = 0;
-    int i = 0;
-    char buf[bsize];
-    string output;
-
-    while((i=read(sock,buf+bytes,bsize-bytes))!=0){
-        bytes+=i;
-    }
-    buf[bytes] = 0;
-
-    string response = buf;
-
-    if(response[response.size()-3]=='.' &&
-       response[response.size()-2]=='\r' &&
-       response[response.size()-1]=='\n'){
-        response[response.size()-3] = 0;
-    }
-
-    return response;
-}
 
